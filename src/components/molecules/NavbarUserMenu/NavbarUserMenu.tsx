ea5b86e0443cb0bc1f4640bd9096ca49e73ee998
Daniel Almeida <quantodaniel@gmail.com>
2023-03-31 19:57:36 -0300

    Update NavbarUserMenu.tsx

--- src/components/molecules/NavbarUserMenu/NavbarUserMenu.tsx
+++ src/components/molecules/NavbarUserMenu/NavbarUserMenu.tsx
@@ -3,7 +3,7 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 const NavbarUserMenu = () => {
   return (
     <button className="flex gap-2 items-center">
-      zuza@materimperium.com
+      my@email.com
       <MdKeyboardArrowDown />
     </button>
   );
