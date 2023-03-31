61e6f4aaa9edfb9e87e421cb8c57e9105b58cdf2
Daniel Almeida <quantodaniel@gmail.com>
2023-03-31 20:18:53 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
  return (
    <>
      <Head>
-       <title>MaterData</title>
+       <title>Simple Table</title>
        <meta name="description" content="MaterData" />
        <meta name="viewport" content="width=device-width, initial-scale=1" />
        <link rel="icon" href="/favicon.ico" />
